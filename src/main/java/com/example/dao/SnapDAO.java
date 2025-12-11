939ee857b8d4c1ebcbec4b6cf456121c5f69aa06
Gooddanddelion <qkrgjsdlf0324@gmail.com>
2025-12-12 02:16:18 +0900

    feat: create SnapDAO.java

--- src/main/java/com/example/dao/SnapDAO.java
+++ src/main/java/com/example/dao/SnapDAO.java
@@ -1,4 +1,18 @@
 package com.example.dao;
 
+import com.example.bean.SnapVO;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class SnapDAO {
+
+    @Autowired
+    SqlSession sqlSession;
+
+    public int insertSnap(SnapVO snapVO) {
+        return sqlSession.insert("snap.insertSnap", snapVO);
+    }
+
 }
