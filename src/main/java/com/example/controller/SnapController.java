c4a1b513848a5d993c5650bf6c89132077be518b
Gooddanddelion <qkrgjsdlf0324@gmail.com>
2025-12-13 15:06:42 +0900

    fix: fix conflict

--- src/main/java/com/example/controller/SnapController.java
+++ src/main/java/com/example/controller/SnapController.java
@@ -4,6 +4,7 @@ import com.example.bean.SnapVO;
 import com.example.dao.SnapDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -67,5 +68,32 @@ public class SnapController {
         snapDAO.insertSnap(snapVO);
 
         return "redirect:/snaps/";
+    }
+
+    @GetMapping("/list")
+    public String snapList(Model model) {
+        model.addAttribute("list", snapDAO.getSnapList());
+        return "list";
+    }
+
+    // 1. 수정 페이지로 이동 (기존 데이터 들고 감)
+    @GetMapping("/edit/{id}")
+    public String editSnap(@PathVariable("id") int id, Model model) {
+        SnapVO snapVO = snapDAO.getSnap(id);
+        model.addAttribute("u", snapVO);
+        return "edit";
+    }
+
+    // 2. 수정 완료 (DB 업데이트 후 목록으로 이동)
+    @PostMapping("/edit/ok")
+    public String editSnapOk(SnapVO snapVO) {
+        snapDAO.updateSnap(snapVO);
+        return "redirect:/snaps/list";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteSnap(@PathVariable("id") int id) {
+        snapDAO.deleteSnap(id);
+        return "redirect:/snaps/list";
     }
 }
